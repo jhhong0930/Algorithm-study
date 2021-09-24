dd90697dbe04298abc9b78f139ea173bcc45bd54
jhhong0930 <jhhong0930@gmail.com>
2021-09-24 23:27:32 +0900

    add 2016년

---
+++ src/week04/P12901.java
@@ -0,0 +1,25 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12901
+// 2016년
+package week04;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class P12901 {
+
+    public String solution(int a, int b) {
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(2016, a-1, b);
+
+        Date date = cal.getTime();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("E", Locale.ENGLISH);
+
+        return sdf.format(date).toUpperCase();
+    }
+
+}
